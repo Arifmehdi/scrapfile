5ddf3d11884cb84d9626eb0e9e199989095e3a7b
Arifmehdi <mehediarif.du@gmail.com>
2024-10-17 18:43:54 +0600

    cleaner py added

--- laboratory/cargurus/helper/cleaner.py
+++ laboratory/cargurus/helper/cleaner.py
@@ -1,8 +1,10 @@
 import csv
+import re
 
 # Input and output CSV file paths
-input_csv = 'input_file.csv'
-output_csv = 'output_file.csv'
+zip_code  = 77007 
+input_csv = f"../public/db/{zip_code}/inventory_info.csv"
+output_csv = f"../public/db/{zip_code}/{zip_code}_output_file.csv"
 
 # Function to clean the price
 def clean_price(price):
@@ -14,8 +16,27 @@ with open(input_csv, mode='r') as infile, open(output_csv, mode='w', newline='')
     writer = csv.writer(outfile)
     
     for row in reader:
+        formatted_number = re.sub(r'\D', '', row[2])
+        row[2] = formatted_number
+        
+        cleaned_price = re.sub(r'[\$,]', '', row[17])
+        row[17] = cleaned_price
+
+        cleaned_miles = re.sub(r'[\$,]', '', row[18])
+        row[18] = cleaned_miles
+
+        cleaned_monthly_pay = re.sub(r'\D', '', row[35])
+        row[35] = cleaned_monthly_pay
+
+        print(formatted_number)
+        print(cleaned_price)
+        print(cleaned_miles)
+        print(cleaned_monthly_pay)
+ 
+
+        
         # Assuming the price is in the first column, change the index accordingly
-        row[0] = clean_price(row[0])
+        # row[0] = clean_price(row[0])
         writer.writerow(row)
 
 print(f"Prices have been cleaned and written to {output_csv}")
