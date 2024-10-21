0bc7799f5722a094151e35e92bf43fb6f956685a
Arifmehdi <mehediarif.du@gmail.com>
2024-10-21 18:16:47 +0600

    make csv

--- laboratory/cargurus/helper/cleaner.py
+++ laboratory/cargurus/helper/cleaner.py
@@ -4,8 +4,10 @@ import re
 # Input and output CSV file paths
 # zip_code  = 77007 
 zip_code  = 75241 
-input_csv = f"../public/db/{zip_code}/inventory_info.csv"
-output_csv = f"../public/db/{zip_code}/{zip_code}_output_file.csv"
+# input_csv = f"../public/db/{zip_code}/inventory_info.csv"
+# output_csv = f"../public/db/{zip_code}/{zip_code}_output_file.csv"
+input_csv = f"../public/db/2/inventory_info.csv"
+output_csv = f"../public/db/2/2_output_file.csv"
 
 # Function to clean the price
 def clean_price(price):
@@ -23,7 +25,7 @@ with open(input_csv, mode='r') as infile, open(output_csv, mode='w', newline='')
         cleaned_price = re.sub(r'[\$,]', '', row[17])
         row[17] = cleaned_price
 
-        cleaned_miles = re.sub(r'[\$,]', '', row[18])
+        cleaned_miles = re.sub(r'[\,mi]', '', row[18])
         row[18] = cleaned_miles
 
         cleaned_monthly_pay = re.sub(r'\D', '', row[35])
